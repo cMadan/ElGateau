0b3ecaaa1a4c4ea564a7c832ac13d23f400c32de
Christopher Madan <cMadan@users.noreply.github.com>
2017-10-27 17:15:22 +0100

    Improved docstrings

--- dev_notebooks/ElGateau.py
+++ dev_notebooks/ElGateau.py
@@ -6,7 +6,7 @@ __author__      = "Christopher Madan"
 __copyright__   = "Copyright 2017, Christopher Madan"
 
 __license__ = "MIT"
-__version__ = "0.0.6"
+__version__ = "0.0.7"
 __maintainer__ = "Christopher Madan"
 __email__ = "christopher.madan@nottingham.ac.uk"
 __status__ = "Development"
@@ -37,24 +37,57 @@ from PIL import Image,ImageDraw,ImageFont
 # functions
 
 def open():
-    """open initial connection to device"""
+    """
+    Open initial connection to Elgato Stream Deck device.
+    
+    Returns
+    ----------
+    elg : USB HID handle
+        Required to pass commands to the device.
+    """
     elg = hid.device(HID_VENDOR, HID_PRODUCT)
     elg.open(HID_VENDOR, HID_PRODUCT)
     return (elg)
 
 
 def reset(elg):
-    """send reset command"""
+    """
+    Send reset command.
+    
+    Inputs
+    ----------
+    elg : USB HID handle
+    """
     elg.send_feature_report(RESET_DATA)
     
 def set_brightness(elg,bright):
-    """set brightness to 'bright'"""
+    """
+    Set brightness of displays.
+    
+    Inputs
+    ----------
+    elg : USB HID handle
+    bright : int, 0-100
+        Brightness value to set LCD display to.
+    """
     BRIGHTNESS_DATA[5] = bright
     elg.send_feature_report(BRIGHTNESS_DATA)
     
     
 def icon_prep(icon,pad=0):
-    """prepare icon (read from file, pad, resize)"""
+    """
+    Prepare icon (read from file, pad, resize).
+    
+    Inputs
+    ----------
+    icon : Filename for icon to prepare, needs to be a PNG in the "icons" folder.
+    bright : px
+        Pad the icon before resizing, this way the icon doesn't go right to edge of display.
+        
+    Returns
+    ----------
+    ico : 72x72 RGBA image
+    """
     # read icon
     ico = Image.open(os.path.join("icons",icon+".png"))
 
@@ -70,7 +103,23 @@ def icon_prep(icon,pad=0):
 
 
 def icon_text(ico,text,font='VeraMono-Bold',col='000000',size=14):
-    """overlay text over icon"""
+    """
+    Overlay text over icon.
+    
+    Inputs
+    ----------
+    ico : 72x72 RGBA image
+        Should have been output from icon_prep or icon_solid.
+    text : str
+        Text to write.
+    font: Font filename, should be in "fonts" folder. Optional.
+    col: Hex color code for text. Optional.
+    size: Font size. Optional.
+    
+    Returns
+    ----------
+    ico : 72x72 RGBA image
+    """
     # underlay
     base = ico
 
@@ -95,14 +144,32 @@ def icon_text(ico,text,font='VeraMono-Bold',col='000000',size=14):
     return (ico)
     
 def icon_solid(col):
-    """create a icon that is a solid color"""
+    """
+    Create a icon that is a solid color.
+    
+    Inputs
+    ----------
+    col: Hex color code for text.
+    
+    Returns
+    ----------
+    ico : 72x72 RGBA image
+    """
     # make blank image of a solid color
     (r,g,b)=bytes.fromhex(col)
     ico = Image.new('RGBA', ICON_SIZE, (r,g,b,255))
     return ico
 
 def key_display(elg,key,ico):
-    """push an icon to a key on the device"""
+    """
+    Push an icon to a key on the device.
+    
+    Inputs
+    ----------
+    elg : USB HID handle
+    key : int, key number on device (1-15)
+    ico : 72x72 RGBA image
+    """
     # icon gets mirrored, so need to flip
     ico = ico.transpose(Image.FLIP_LEFT_RIGHT)
 
@@ -130,7 +197,19 @@ def key_display(elg,key,ico):
     elg.write(msg)
 
 def key_remap(key):
-    """remaps key numbers to a more intuitive order"""
+    """
+    Remaps key numbers to a more intuitive order.
+    
+    Inputs
+    ----------
+    key : int, key number on device (1-15)
+    (5,4,3,2,1,10,9,8,7,6,15,14,13,12,11)
+
+    Returns
+    ----------
+    key : int, key number on device (1-15)
+    (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)
+    """
     key = (np.floor((key-1)/5))*5 + (5-(np.mod(key-1,5)))
     return (int(key))
 
